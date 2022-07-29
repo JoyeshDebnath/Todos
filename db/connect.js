9060f7ffc3bc563733b4793b874fabdd8f863766
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2022-07-29 10:17:50 +0530

    buggs

--- db/connect.js
+++ db/connect.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 // const { Schema } = mongoose;
 require("dotenv").config();
 // get the connection string of the mongodb
-const connectionString = process.env.connectionString;
+// const connectionString = process.env.connectionString;
 
 const connectDB = (url) => {
-  return mongoose.connect(url);
+	console.log(url);
+	return mongoose.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
 };
 
 module.exports = connectDB;
